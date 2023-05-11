866115a21757426973bec4e9d12402a459d20383
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-05-11 13:41:08 -0300

    Aprendendo TypeScript.

--- ex001/app.js
+++ ex001/app.js
@@ -1,1 +1,14 @@
-console.log('Your code goes here!!!');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result); //dará um erro se colocarmos diretamente (phrase + n1 + n2) pois entende como se todos fossem strings, portanto é necessário criar a variável
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5; // == 5.0
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
---
+++ ex001/basics.ts
@@ -0,0 +1,15 @@
+function add(n1: number, n2: number, showResult: boolean, phrase: string) { 
+    let result = n1 + n2
+    if (showResult) {
+        console.log(phrase + result); //dará um erro se colocarmos diretamente (phrase + n1 + n2) pois entende como se todos fossem strings, portanto é necessário criar a variável
+    } else {
+    return n1 + n2
+    }
+}
+
+const number1 = 5; // == 5.0
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
+
+add(number1, number2, printResult, resultPhrase)
